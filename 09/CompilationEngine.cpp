12c50cedb267ca0519934e700b3ef732d3bc6ad9
lxg <l1753531985@gmail.com>
2021-08-16 07:42:00 -0400

    只差输出到文本

--- 09/CompilationEngine.cpp
+++ 09/CompilationEngine.cpp
@@ -6,7 +6,29 @@ int main()
 	while (token.hasMoreTokens())
 	{
 		token.advance();
-		cout << token.type2Str(token.tokenType()) << endl;
+		switch (token.tokenType())
+		{
+			case TokenType::KEYWORD:
+				cout << "<keyword> " << token.keyword() << " </keyword>" << endl;
+				break;
+			case TokenType::SYMBOL:
+				cout << "<symbol> " << token.symbol() << " </symbol>" << endl;
+				break;
+			case TokenType::IDENTIFIER:
+				cout << "<identifier> " << token.identifier() << " </identifier>" << endl;
+				break;
+			case TokenType::INT_CONST:
+				cout << "<integerConstant> " << token.intVal() << " </integerConstant>" << endl;
+				break;
+			case TokenType::STRING_CONST:
+				cout << "<stringConstant> " << token.stringVal() << " </stringConstant>" << endl;
+				break;
+			default:
+				cout << "No any token type" << endl;
+				break;
+
+		}
+		//cout << token.type2Str(token.tokenType()) << endl;
 		//cout << token.getWord() << endl; 
 	}
 	return 0;
