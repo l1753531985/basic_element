41b54db61c2d7dc117fdf22175714037db566de1
lxg <l1753531985@gmail.com>
2021-09-02 06:01:33 -0400

    框架完成，需要bug调试

--- 09/CompilationEngine.cpp
+++ 09/CompilationEngine.cpp
@@ -1,10 +1,12 @@
 #include "CompilationEngine.h"
 
 CompilationEngine::CompilationEngine(const queue<pair<string, string>>& tokens, int val)
-	: compileByToken{new unordered_map<string, CompileType>}, indentationSize{val}
+	: compileByToken{new unordered_map<string, CompileType>}, compileByTag{new unordered_map<string, TagType>}, ops{new unordered_set<string>}, indentationSize{val}
 {
 	this->tokens = tokens;
-	compileByToken->insert({{"static", CLASSVARDEC}, {"field", CLASSVARDEC}, {"constructor", SUBROUTINEDEC}, {"function", SUBROUTINEDEC}, {"method", SUBROUTINEDEC}, {"var", VAR}, {"if", IF}, {"while", WHILE}, {"do", DO}, {"return", RETURN}});
+	compileByToken->insert({{"static", CLASSVARDEC}, {"field", CLASSVARDEC}, {"constructor", SUBROUTINEDEC}, {"function", SUBROUTINEDEC}, {"method", SUBROUTINEDEC}, {"var", VAR}, {"if", IF}, {"while", WHILE}, {"do", DO}, {"return", RETURN}, {"let", LET}});
+	compileByTag->insert({{"keyword", T_KEYWORD}, {"symbol", T_SYMBOL}, {"identifier", T_IDENTIFIER}, {"int_const", T_INI_CONST}, {"string_const", T_STRING_CONST}});
+	ops->insert({"+", "-", "*", "/", "&", "|", "<", ">", "="});
 	CompileClass(cout, 0);
 }
 
@@ -15,7 +17,7 @@ CompilationEngine::~CompilationEngine()
 
 void CompilationEngine::printTokenInXml(ostream& os, int indentation)
 {
-	os << setw(indentation) << "<" << tokens.front().first << ">" ;
+	os << setw(indentation) << " " << "<" << tokens.front().first << ">" ;
 	os << " " << tokens.front().second << " ";
 	os << "</" << tokens.front().first << ">" << endl;
 }
@@ -49,6 +51,13 @@ CompileType CompilationEngine::token2Type(string token)
 	return iter->second;
 }
 
+TagType CompilationEngine::tag2Type(string tag)
+{
+	unordered_map<string, TagType>::iterator iter = compileByTag->find(tag);
+	if (iter == compileByTag->end()) return TagType::T_NONE;
+	return iter->second;
+}
+
 //fot test
 void CompilationEngine::printAllTokens()
 {
@@ -78,12 +87,11 @@ void CompilationEngine::popBeforeFlag(string flag)
 
 void CompilationEngine::CompileClass(ostream& os, int indentation)
 {
-	os << "<class>" << endl;
+	os << setw(indentation) << " " << "<class>" << endl;
 	advanceUntilFlag(os, "{", indentation+indentationSize);
 	bool loopFlag = true;
 	while (loopFlag)
 	{
-		sleep(1);
 		switch (token2Type(tokens.front().second))
 		{
 			case CLASSVARDEC:
@@ -100,24 +108,30 @@ void CompilationEngine::CompileClass(ostream& os, int indentation)
 		}
 	}
 	advanceUntilFlag(os, "}", indentation+indentationSize);
-	os << "</class>" << endl;
+	os << setw(indentation) << " "  << "</class>" << endl;
 }
 
 void CompilationEngine::CompileClassVarDec(ostream& os, int indentation)
 {
-	cout << "indentation: " << indentation << endl;
-	os << setw(indentation) << "<CompileClassVarDec>" << endl;
+	os << setw(indentation) << " " << "<classVarDec>" << endl;
 	advanceUntilFlag(os, ";", indentation+indentationSize);
-	os << setw(indentation) << "</CompileClassVarDec>" << endl;
+	os << setw(indentation) << " " << "</classVarDec>" << endl;
 }
 
 void CompilationEngine::CompileSubroutineDec(ostream& os, int indentation)
 {
-	os << setw(indentation) << "<CompileSubroutineDec>" << endl;
-	advanceUntilFlag(os, "(", indentation);
+	os << setw(indentation) << " " << "<subroutineDec>" << endl;
+	advanceUntilFlag(os, "(", indentation+indentationSize);
 	CompileParameterList(os, indentation+indentationSize);
-	os << setw(indentation) << "<subroutineBody>" << endl;
-	advanceUntilFlag(os, "{", indentation);
+	advanceUntilFlag(os, ")", indentation+indentationSize);
+	CompileSubroutineBody(os, indentation+indentationSize);
+	os << setw(indentation) << " " << "</subroutineDec>" << endl;
+}
+
+void CompilationEngine::CompileSubroutineBody(ostream& os, int indentation)
+{
+	os << setw(indentation) << " " << "<subroutineBody>" << endl;
+	advanceUntilFlag(os, "{", indentation+indentationSize);
 	bool loopFlag = true;
 	while (loopFlag)
 	{
@@ -140,27 +154,231 @@ void CompilationEngine::CompileSubroutineDec(ostream& os, int indentation)
 				break;
 		}
 	}
-	advanceUntilFlag(os, "}", indentation);
-	os << setw(indentation) << "</subroutineBody>" << endl;
-	os << setw(indentation) << "</CompileSubroutineDec>" << endl;
+	advanceUntilFlag(os, "}", indentation+indentationSize);
+	os << setw(indentation) << " " << "</subroutineBody>" << endl;
 }
 
 void CompilationEngine::CompileParameterList(ostream& os, int indentation)
 {
-	os << setw(indentation) << "<parameterList>";
+	os << setw(indentation) << " " << "<parameterList>";
 	advanceBeforeFlag(os, ")", indentation);
-	os << "</parameterList>" << endl;
+	os << " " << "</parameterList>" << endl;
 }
 
 void CompilationEngine::CompileVarDec(ostream& os, int indentation)
 {
-	os << setw(indentation) << "<varDec>" << endl;
-	advanceUntilFlag(os, ";", indentation);
-	os << setw(indentation) << "</varDec>" << endl;
+	os << setw(indentation) << " " << "<varDec>" << endl;
+	advanceUntilFlag(os, ";", indentation+indentationSize);
+	os << setw(indentation) << " " << "</varDec>" << endl;
 }
 
 void CompilationEngine::CompileStatements(ostream& os, int indentation)
 {
-	os << setw(indentation) << "<CompileStatements>" << endl;
-	os << setw(indentation) << "</CompileStatements>" << endl;
+	os << setw(indentation) << " " << "<statements>" << endl;
+	bool loopFlag = true;
+	while (loopFlag)
+	{
+		switch (token2Type(tokens.front().second))
+		{
+				case CompileType::LET:
+					CompileLet(os, indentation+indentationSize);
+					break;
+				case CompileType::IF:
+					CompileIf(os, indentation+indentationSize);
+					break;
+				case CompileType::WHILE:
+					CompileWhile(os, indentation+indentationSize);
+					break;
+				case CompileType::DO:
+					CompileDo(os, indentation+indentationSize);
+					break;
+				case CompileType::RETURN:
+					CompileReturn(os, indentation+indentationSize);
+					break;
+				default:
+					loopFlag = false;
+					break;
+		}
+	}
+	os << setw(indentation) << " " << "</statements>" << endl;
+}
+
+void CompilationEngine::CompileLet(ostream& os, int indentation)
+{
+	os << setw(indentation) << " " << "<letStatement>" << endl;
+	// output keyword and varname
+	printTokenInXml(os, indentation+indentationSize);
+	tokens.pop();
+	printTokenInXml(os, indentation+indentationSize);
+	tokens.pop();
+
+	if (tokens.front().second == "[")
+	{
+		advanceUntilFlag(os, "[", indentation+indentationSize);
+		CompileExpression(os, indentation+indentationSize);
+		advanceUntilFlag(os, "]", indentation+indentationSize);
+	}
+	advanceUntilFlag(os, "=", indentation+indentationSize);
+	CompileExpression(os, indentation+indentationSize);
+	advanceUntilFlag(os, ";", indentation+indentationSize);
+	os << setw(indentation) << " " << "</letStatement>" << endl;
+}
+
+void CompilationEngine::CompileWhile(ostream& os, int indentation)
+{
+	os << setw(indentation) << " " << "<whileStatement>" << endl;
+	advanceUntilFlag(os, "(", indentation+indentationSize);
+	CompileExpression(os, indentation+indentationSize);
+	advanceUntilFlag(os, "{", indentation+indentationSize);
+	CompileStatements(os, indentation+indentationSize);
+	advanceUntilFlag(os, "}", indentation+indentationSize);
+	os << setw(indentation) << " " << "</whileStatement>" << endl;
+}
+
+void CompilationEngine::CompileDo(ostream& os, int indentation)
+{
+	os << setw(indentation) << " " << "<doStatement>" << endl;
+	//print keyword
+	printTokenInXml(os, indentation+indentationSize);
+	tokens.pop();
+	CompileSubroutineCall(os, indentation+indentationSize);	
+	advanceUntilFlag(os, ";", indentation+indentationSize);
+	os << setw(indentation) << " " << "</doStatement>" << endl;
+}
+
+void CompilationEngine::CompileReturn(ostream& os, int indentation)
+{
+	os << setw(indentation) << " " << "<returnStatement>" << endl;
+	// print keyword
+	printTokenInXml(os, indentation+indentationSize);
+	tokens.pop();
+	if (tokens.front().second != ";")
+		CompileExpression(os, indentation+indentationSize);
+	// print ; 
+	printTokenInXml(os, indentation+indentationSize);
+	tokens.pop();
+	os << setw(indentation) << " " << "</returnStatement>" << endl;
+}
+
+void CompilationEngine::CompileIf(ostream& os, int indentation)
+{
+	os << setw(indentation) << " " << "<IfStatement>" << endl;
+	//print keyword
+	printTokenInXml(os, indentation+indentationSize);
+	tokens.pop();
+
+	// if branch operate
+	advanceUntilFlag(os, "(", indentation+indentationSize);
+	CompileExpression(os, indentation+indentationSize);
+	advanceUntilFlag(os, "{", indentation+indentationSize);
+	CompileStatements(os, indentation+indentationSize);
+	advanceUntilFlag(os, "}", indentation+indentationSize);
+
+	// else branch operate
+	if (tokens.front().second == "else")
+	{
+		//print keyword
+		printTokenInXml(os, indentation+indentationSize);
+		tokens.pop();
+		advanceUntilFlag(os, "{", indentation+indentationSize);
+		CompileStatements(os, indentation+indentationSize);
+		advanceUntilFlag(os, "}", indentation+indentationSize);
+	}
+
+	os << setw(indentation) << " " << "</IfStatement>" << endl;
+}
+
+void CompilationEngine::CompileExpression(ostream& os, int indentation)
+{
+	os << setw(indentation) << " " << "<expression>" << endl;
+	CompileTerm(os, indentation+indentationSize);
+	while (ops->find(tokens.front().first) != ops->end())
+	{
+		printTokenInXml(os, indentation+indentationSize);
+		tokens.pop();
+		CompileTerm(os, indentation+indentationSize);
+	}
+	os << setw(indentation) << " " << "</expression>" << endl;
+}
+
+void CompilationEngine::CompileTerm(ostream& os, int indentation)
+{
+	os << setw(indentation) << " " << "<term>" << endl;
+	switch(tag2Type(tokens.front().first))
+	{
+		case T_KEYWORD:
+		case T_INI_CONST:
+		case T_STRING_CONST:
+			printTokenInXml(os, indentation+indentationSize);
+			tokens.pop();
+			break;
+		case T_IDENTIFIER:
+			printTokenInXml(os, indentation+indentationSize);
+			tokens.pop();
+			if (tokens.front().second == "[")
+			{
+				advanceUntilFlag(os, "[", indentation+indentationSize);
+				CompileExpression(os, indentation+indentationSize);
+				advanceUntilFlag(os, "]", indentation+indentationSize);
+			}
+			else if (tokens.front().second == "(")
+			{
+				advanceUntilFlag(os, "(", indentation+indentationSize);
+				CompileExpressionList(os, indentation+indentationSize);
+				advanceUntilFlag(os, ")", indentation+indentationSize);
+			}
+			else if (tokens.front().second == ".")
+			{
+				CompileSubroutineCall(os, indentation+indentationSize);
+			}
+			break;
+		case T_SYMBOL: 
+			if (tokens.front().second == "(")
+			{
+				advanceUntilFlag(os, "(", indentation+indentationSize);
+				CompileExpression(os, indentation+indentationSize);
+				advanceUntilFlag(os, ")", indentation+indentationSize);
+			}
+			else if (tokens.front().second == "-" || tokens.front().second == "~")
+			{
+				printTokenInXml(os, indentation+indentationSize);
+				tokens.pop();
+				CompileTerm(os, indentation+indentationSize);
+			}
+		default:
+			break;
+	}
+	os << setw(indentation) << " " << "</term>" << endl;
+}
+
+void CompilationEngine::CompileSubroutineCall(ostream& os, int indentation)
+{
+	printTokenInXml(os, indentation);
+	tokens.pop();
+	if (tokens.front().second == ".")
+	{
+		advanceUntilFlag(os, ".", indentation);
+		printTokenInXml(os, indentation);
+		tokens.pop();
+	}
+	advanceUntilFlag(os, "(", indentation);
+	CompileExpressionList(os, indentation);
+	advanceUntilFlag(os, ")", indentation);
+}
+
+void CompilationEngine::CompileExpressionList(ostream& os, int indentation) 
+{
+	os << setw(indentation) << " " << "<expressionList>";
+	if (tokens.front().second == ")") 
+	{
+		os << " " << "</expressionList>" << endl;
+		return;
+	}
+	CompileExpression(os, indentation+indentationSize);
+	while (tokens.front().second == ",")
+	{
+		advanceUntilFlag(os, ",", indentation+indentationSize);	
+		CompileExpression(os, indentation+indentationSize);
+	}
+	os << " " << "</expressionList>" << endl;
 }
--- 09/CompilationEngine.h
+++ 09/CompilationEngine.h
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <queue> 
 #include <unordered_map>
+#include <unordered_set>
 #include <iomanip>
 #include <unistd.h>
 
@@ -9,14 +10,20 @@ using namespace std;
 
 enum CompileType {CLASSVARDEC, SUBROUTINEDEC, VAR, LET, IF, WHILE, DO, RETURN, NONE};
 
+enum TagType {T_KEYWORD, T_INI_CONST, T_STRING_CONST, T_IDENTIFIER, T_SYMBOL, T_NONE};
+
 class CompilationEngine {
 private:
 	queue<pair<string, string>> tokens;
 	unordered_map<string, CompileType>* compileByToken;
+	unordered_map<string, TagType>* compileByTag;
+	unordered_set<string>* ops;
+	void CompileSubroutineCall(ostream&, int);
 	void advanceUntilFlag(ostream&, string, int);
 	void advanceBeforeFlag(ostream&, string, int);
 	void printTokenInXml(ostream&, int);
 	CompileType token2Type(string);
+	TagType tag2Type(string);
 	// for test
 	void popUntilFlag(string);
 	// for test
@@ -28,9 +35,18 @@ public:
 	void CompileClass(ostream&, int);
 	void CompileClassVarDec(ostream&, int);
 	void CompileSubroutineDec(ostream&, int);
+	void CompileSubroutineBody(ostream&, int);
 	void CompileParameterList(ostream&, int);
 	void CompileVarDec(ostream&, int);
 	void CompileStatements(ostream&, int);
+	void CompileLet(ostream&, int);
+	void CompileDo(ostream&, int);
+	void CompileWhile(ostream&, int);
+	void CompileReturn(ostream&, int);
+	void CompileIf(ostream&, int);
+	void CompileExpression(ostream&, int);
+	void CompileTerm(ostream&, int); 
+	void CompileExpressionList(ostream&, int);
 	//fot test
 	void printAllTokens();
 };
